abdfdc4e945624a01d9ec84015b57c855d61e2f0
vickycj <vygneshwaran@gmail.com>
2020-01-31 11:09:20 +0530

    Initial commit

---
+++ src/main/java/com/gamification/digigame/DigiGameApplication.java
@@ -0,0 +1,13 @@
+package com.gamification.digigame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamification/digigame/DigiGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamification.digigame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
